e1deb687cb6d73ed4782d1a6fb7c17278bb2129f
mary <laguta113@gmail.com>
2023-11-20 14:30:16 +0600

    Commit with a solution

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -3,12 +3,12 @@ package school.mjc.stage0.module3.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
         double a = 1;
-        double b = 1;
+        double b = 2;
 
-        double leftPart = 9 * Math.pow(a, 2) - 5 * b + 2 + a - 7;
-        double rightPart = (a + b - 4 * a * b) / 2;
-        double result = leftPart * rightPart;
+        double result = ((9 * (a * a)) - (5 * b)) + 2 + a - 7;
+        double result2 = (a + b - (4 * a * b)) / 2;
+        double result3 = result * result2;
 
-        System.out.println("The result of the formula is: " + result);
+        System.out.println(result3);
     }
 }
